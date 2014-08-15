eac76e25152492abf0ac1a001d89a956dba0af23
Åshild Thorrud <ashild.thorrud@gmail.com>
2014-08-15 17:36:29 +0200

    Endrer bilde fra facebook til google ved å trykke på det

--- src/main/webapp/Assets/Scripts/csp-demo.js
+++ src/main/webapp/Assets/Scripts/csp-demo.js
@@ -13,4 +13,10 @@
         $("#profiles").click(function() {
                     $("span").html(localStorage["name"]).val();
         });
+    $('#profilePic').on({
+    'click': function(){
+       $("#profilePic").attr("src","https://lh4.googleusercontent.com/-f7Enxliyigc/TiMPtJ0zsSI/AAAAAAAABEo/Cra5KP-oy0I/w462-h460-no/Thorrud%2B%C3%85shild%2BKaldal.JPG");
+    }
     });
+
+});

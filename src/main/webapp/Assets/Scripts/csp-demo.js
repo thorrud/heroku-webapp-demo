f80bc23f8ed66c2b0d1ea20960a65ac1a81113b7
ashild.thorrud@gmail.com <ashildkaldahlthorrud@Ashilds-MacBook-Pro.local>
2014-08-16 17:18:11 +0200

    La til facebook som tillatt bildekilde

--- src/main/webapp/Assets/Scripts/csp-demo.js
+++ src/main/webapp/Assets/Scripts/csp-demo.js
@@ -2,21 +2,23 @@
 
 
     $(document).ready(function() {
-    $("#csp-submit").submit(function(event)
-        {
-
-            $("input:first").val();
-            localStorage["name"] = $("input:first").val();
-            event.preventDefault();
-            return true;
-        });
+    
         $("#profiles").click(function() {
-                    $("span").html(localStorage["name"]).val();
+            $("#csp-submit").submit(function(event) {
+
+                $("input:first").val();
+                localStorage["name"] = $("input:first").val();
+                event.preventDefault();
+                return true;
+            });
+            
+            $("span").html(localStorage["name"]).val();
         });
+
     $('#profilePic').on({
-    'click': function(){
-       $("#profilePic").attr("src","https://lh4.googleusercontent.com/-f7Enxliyigc/TiMPtJ0zsSI/AAAAAAAABEo/Cra5KP-oy0I/w462-h460-no/Thorrud%2B%C3%85shild%2BKaldal.JPG");
-    }
+        'click': function(){
+           $("#profilePic").attr("src","https://lh4.googleusercontent.com/-f7Enxliyigc/TiMPtJ0zsSI/AAAAAAAABEo/Cra5KP-oy0I/w462-h460-no/Thorrud%2B%C3%85shild%2BKaldal.JPG");
+        }
     });
 
 });

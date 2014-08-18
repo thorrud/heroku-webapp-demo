9fe8c73960fd56295e71aeb35bcfbdd807c73429
Mikael Strand <mikael.strand@capgemini.com>
2014-08-18 16:07:47 +0200

    Added prettier pictures

--- src/main/webapp/Assets/Scripts/csp-demo.js
+++ src/main/webapp/Assets/Scripts/csp-demo.js
@@ -18,7 +18,7 @@
 
     $('#profilePic').on({
         'click': function(){
-           $("#profilePic").attr("src","https://lh4.googleusercontent.com/-f7Enxliyigc/TiMPtJ0zsSI/AAAAAAAABEo/Cra5KP-oy0I/w462-h460-no/Thorrud%2B%C3%85shild%2BKaldal.JPG");
+           $("#profilePic").attr("src","http://gifts.worldwildlife.org/gift-center/Images/large-species-photo/large-Rockhopper-Penguin-photo.jpg");
         }
     });
 
